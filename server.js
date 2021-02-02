415cb9fa5538d36933a66e96aaa008b1f72b8683
Hermann Marx <marxhermann@web.de>
2021-02-02 17:38:32 +0100

    update

--- server.js
+++ server.js
@@ -17,4 +17,21 @@ app.get("/orders", orders);
 
 app.get("/orders/:id", orders);
 
+app.delete("/users/delete/:id", (req, res) => {
+  const { id } = req.params.id;
+  try {
+    const data = pool.query("DELETE FROM users WHERE id=$1", [id]);
+
+    res.json({
+      code: 201,
+      operation: "success",
+      description: "Deleted order by id",
+      data: data.rows,
+    });
+  } catch (e) {
+    console.error(Error(e));
+    res.status(500).send("Something happened");
+  }
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
