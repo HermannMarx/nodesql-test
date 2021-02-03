83d0c3bbafe94baa4ad418ffa05e21c8d3465e20
Hermann Marx <marxhermann@web.de>
2021-02-03 14:39:22 +0100

    added post, put, delete

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const pool = require("../dbconfig");
 
-router.get("/orders", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const data = await pool.query("SELECT * FROM orders");
 
@@ -19,7 +19,7 @@ router.get("/orders", async (req, res) => {
   }
 });
 
-router.get("/orders/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const data = await pool.query("SELECT * FROM orders WHERE id=$1", [id]);
@@ -32,8 +32,55 @@ router.get("/orders/:id", async (req, res) => {
     });
   } catch (e) {
     console.error(Error(e));
-    res.status(500).send("Something happened");
+    res.status(500).send("Something happened,Hermann");
   }
+});
+
+router.post("/", (req, res) => {
+  const { price, date, user_id } = req.query; // We retrieve the id from the form (body-parser)
+
+  //console.log(req);
+  console.log(req.query);
+  console.log(req.query.price);
+  console.log(req.query.date);
+  console.log(req.query.user_id);
+
+  pool
+    .query("INSERT INTO orders(price, date, user_id) values($1, $2, $3);", [
+      price,
+      date,
+      user_id,
+    ]) // We inject the name in the request
+    .then((data) => res.status(201).json(data))
+    .catch((e) => res.sendStatus(404)); // In case of problem we send an HTTP code
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { price, date, user_id } = req.query; // We retrieve the id from the form (body-parser)
+
+  //console.log(req);
+  console.log(req.query);
+  console.log(req.query.price);
+  console.log(req.query.date);
+  console.log(req.query.user_id);
+  //UPDATE users SET first_name=$1, last_name=$2, age=$3 WHERE id=$4 RETURNING *;
+  pool
+    .query(
+      "UPDATE orders SET price=$1, date=$2, user_id=$3 WHERE id=$4 RETURNING*;",
+      [price, date, user_id, id]
+    ) // We inject the name in the request
+    .then((data) => res.status(201).json(data))
+    .catch((e) => res.sendStatus(404)); // In case of problem we send an HTTP code
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  //const { price, date, user_id } = req.query; // We retrieve the id from the form (body-parser)
+  pool
+    .query("DELETE FROM orders WHERE id=$1;", [id]) // We inject the name in the request
+    .then((data) => res.status(201).json(data))
+    .catch((e) => res.sendStatus(404)); // In case of problem we send an HTTP code
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,29 +9,14 @@ const { PORT } = process.env;
 const app = express();
 const port = PORT;
 
-app.get("/users", users);
+app.use("/users", users);
 
-app.get("/users/:id", users);
+//app.get("/users/:id", users);
 
-app.get("/orders", orders);
+//app.delete("users/:id", users);
 
-app.get("/orders/:id", orders);
+app.use("/orders", orders);
 
-app.delete("/users/delete/:id", (req, res) => {
-  const { id } = req.params.id;
-  try {
-    const data = pool.query("DELETE FROM users WHERE id=$1", [id]);
-
-    res.json({
-      code: 201,
-      operation: "success",
-      description: "Deleted order by id",
-      data: data.rows,
-    });
-  } catch (e) {
-    console.error(Error(e));
-    res.status(500).send("Something happened");
-  }
-});
+//app.get("/orders/:id", orders);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
